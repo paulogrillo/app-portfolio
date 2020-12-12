1cc7b1e1fede54a20f1e671439d55329d0819f7b
Paulo <paulodigitalart@gmail.com>
2020-12-12 04:26:36 -0300

    0-3

--- components/projetos/Projetos.jsx
+++ components/projetos/Projetos.jsx
@@ -2,30 +2,26 @@ import react from 'react'
 import React from 'react'
 import { AiOutlineCode } from 'react-icons/ai'
 import Faixa from '../partials/Faixa';
+import CardProjetos from '../partials/CardProjetos';
 
 export default function Projetos(){
     return(
-        <div className="space-y-10">
+        <div className="space-y-10 ">
                       <Faixa />  
-            <div className=" ">
-            <div class="grid gap-4 grid-cols-2">
-
-                <div className="text-sm w-360">
-                    <img src="/google.jpg" alt=""/>TextoTextoTextoTextoTextoTexto</div>
-                <div>
-                    <img src="/google.jpg" alt=""/></div>
-                <div>
-                    <img src="/google.jpg" alt=""/></div>
-                <div>
-                    <img src="/google.jpg" alt=""/></div>
-                <div>
-                    <img src="/google.jpg" alt=""/></div>
-                <div>
-                    <img src="/google.jpg" alt=""/></div>
-            </div>   
+                      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight  text-red-500 sm:text-4xl py-8 text-center px-20 ">
+        Hard Skils
+      </p>
+     
+        <div class="lg:text-center">
+          
+              <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            Marketing, Performance & Transformação Digital.
+          </p>
         </div>
-        </div>
-
+        <CardProjetos />
+  </div>
+</div>
 
     )
 }
