d15381275100169a6f9b483bf1285d30ea3ae7c3
paulogrillo <paulodigitalart@gmail.com>
2021-08-28 03:15:19 -0300

    update

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const { nextI18NextRewrites } = require('next-i18next/rewrites')
-
-const localeSubpaths = {
-    hr: 'hr'
-}
-
-module.exports = {
-    rewrites: async () => nextI18NextRewrites(localeSubpaths), 
-}
